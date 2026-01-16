aaab962661c5cf5d4879cd164640fa176c7c9f15
kamodakara <kamodeve@gmail.com>
2026-01-16 15:48:03 +0900

    画面追加

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,69 @@ use rand::Rng;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_state::<GameState>()
         .add_systems(Startup, setup)
-        .add_systems(Update, player_input_system)
-        .add_systems(Update, battle_end_check_system)
-        .add_systems(Update, ui_update_system)
-        .add_systems(Update, ui_update_enemy_system)
-        .add_systems(Update, ui_update_enemy_damage_popup_system)
-        .add_systems(Update, ui_update_player_status_system)
-        .add_systems(Update, ui_update_command_system)
-        .add_systems(Update, ui_update_message_system)
-        .add_systems(Update, ui_update_skill_effect_system)
-        .add_systems(Update, boss_slain_banner_system)
+        .add_systems(OnEnter(GameState::Preparation), setup_preparation_screen)
+        .add_systems(
+            Update,
+            preparation_button_system.run_if(in_state(GameState::Preparation)),
+        )
+        .add_systems(OnExit(GameState::Preparation), cleanup_preparation_screen)
+        .add_systems(OnEnter(GameState::Battle), setup_battle_screen)
+        .add_systems(
+            Update,
+            player_input_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            battle_end_check_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(Update, ui_update_system.run_if(in_state(GameState::Battle)))
+        .add_systems(
+            Update,
+            ui_update_enemy_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            ui_update_enemy_damage_popup_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            ui_update_player_status_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            ui_update_command_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            ui_update_message_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            ui_update_skill_effect_system.run_if(in_state(GameState::Battle)),
+        )
+        .add_systems(
+            Update,
+            boss_slain_banner_system.run_if(in_state(GameState::Battle)),
+        )
         .run();
 }
 
 // ================== Components & Resources ==================
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash, Default)]
+enum GameState {
+    #[default]
+    Preparation, // 準備画面
+    Battle, // 戦闘画面
+}
+
+#[derive(Component)]
+struct PreparationScreen;
+
+#[derive(Component)]
+struct StartBattleButton;
+
 #[derive(Resource, PartialEq, Eq)]
 enum BattlePhase {
     DecideEnemyConduct, // 敵行動決定
@@ -710,8 +758,127 @@ enum BannerPhase {
 struct BattleResource(Battle);
 
 // ================== Setup ==================
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+// 準備画面のセットアップ
+fn setup_preparation_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/x12y16pxMaruMonica.ttf");
+
+    commands
+        .spawn((
+            PreparationScreen,
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Px(40.0),
+                ..default()
+            },
+            BackgroundColor(Color::from(LinearRgba {
+                red: 0.1,
+                green: 0.1,
+                blue: 0.15,
+                alpha: 1.0,
+            })),
+        ))
+        .with_children(|parent| {
+            // タイトル
+            parent.spawn((
+                Text::new("戦闘準備"),
+                TextFont {
+                    font: font.clone(),
+                    font_size: 48.0,
+                    ..default()
+                },
+                TextColor(Color::WHITE),
+            ));
+
+            // 戦闘開始ボタン
+            parent
+                .spawn((
+                    StartBattleButton,
+                    Button,
+                    Node {
+                        width: Val::Px(300.0),
+                        height: Val::Px(80.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        border: UiRect::all(Val::Px(2.0)),
+                        ..default()
+                    },
+                    BackgroundColor(Color::from(LinearRgba {
+                        red: 0.25,
+                        green: 0.25,
+                        blue: 0.35,
+                        alpha: 1.0,
+                    })),
+                    BorderColor::all(Color::WHITE),
+                ))
+                .with_children(|button| {
+                    button.spawn((
+                        Text::new("戦闘開始"),
+                        TextFont {
+                            font: font.clone(),
+                            font_size: 32.0,
+                            ..default()
+                        },
+                        TextColor(Color::WHITE),
+                    ));
+                });
+        });
+}
+
+// 準備画面のクリーンアップ
+fn cleanup_preparation_screen(
+    mut commands: Commands,
+    query: Query<Entity, With<PreparationScreen>>,
+) {
+    for entity in query.iter() {
+        if let Ok(mut entity_commands) = commands.get_entity(entity) {
+            entity_commands.despawn();
+        }
+    }
+}
+
+// ボタンクリックで戦闘開始
+fn preparation_button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<StartBattleButton>),
+    >,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                next_state.set(GameState::Battle);
+            }
+            Interaction::Hovered => {
+                *color = BackgroundColor(Color::from(LinearRgba {
+                    red: 0.35,
+                    green: 0.35,
+                    blue: 0.45,
+                    alpha: 1.0,
+                }));
+            }
+            Interaction::None => {
+                *color = BackgroundColor(Color::from(LinearRgba {
+                    red: 0.25,
+                    green: 0.25,
+                    blue: 0.35,
+                    alpha: 1.0,
+                }));
+            }
+        }
+    }
+}
+
+// 戦闘画面のセットアップ
+fn setup_battle_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(BattlePhase::DecideEnemyConduct);
     commands.insert_resource(Turn(1));
     // 初期ログと敵行動決定
